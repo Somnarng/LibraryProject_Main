77c9265a954bce03079ddb4e8fbad1d1354801c5
Somnarng <99498786+Somnarng@users.noreply.github.com>
2024-01-03 13:13:36 -0500

    Updated House + Scene transition testing
    -Decreased the size of the test scene
    -Added furniture to test layer sorting
    -Added test "door" to leave the test scene

---
+++ Assets/Game/Scripts/Interaction/UnityEventInteract.cs
@@ -0,0 +1,26 @@
+using MoreMountains.Tools;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UnityEventInteract : MonoBehaviour, IInteractable
+{
+    [SerializeField] private bool interactable;
+    [SerializeField] private string interactText;
+    [SerializeField] public UnityEvent unityEvent;
+    [SerializeField] private float delay = 0f;
+    public void Interact()
+    {
+        Invoke("EventTrigger", delay);
+    }
+
+   public void EventTrigger()
+    {
+        unityEvent.Invoke();
+    }
+
+    public bool Interactable { get { return interactable; } set { interactable = value; } }
+    public string InteractText { get { return interactText; } set { interactText = value; } }
+    public Transform objectTransform { get => gameObject.transform; }
+}
